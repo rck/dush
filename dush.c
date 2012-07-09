ca2d7129cc5873298f4c0a1c78d351d5ee969d89
rck <dev.rck@gmail.com>
2012-07-09 16:06:42 +0200

    moved to external uthash

--- dush.c
+++ dush.c
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <ftw.h>
 
 /* "external" sources */
-#include "uthash.h"
+#include <uthash.h>
 
 /* globals */
 unsigned int width = 77; /* 80 - "FILE MODE" (1 char) -  [] */
