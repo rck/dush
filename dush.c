6fef1d36bbfe235dbc764cb5e6d4d4e0af6cf64a
rck <dev.rck@gmail.com>
2012-07-10 11:26:12 +0200

    added a check for FTW_ACTIONRETVAL, removed _GNU_SOURCE define from source

--- dush.c
+++ dush.c
@@ -13,7 +13,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define _GNU_SOURCE /* we need it for glibc specific nftw stuff */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
